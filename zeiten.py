6bb8dd048bfff9513bf93ee0635331707138efdb
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-25 11:51:11 +0200

    e

--- zeiten.py
+++ zeiten.py
@@ -1,24 +1,31 @@
 # zeiten.py
-def zeitInSekunden(h, m, s):
+def zeitInSekunden(Zeit):
     gesamt = 0
+    zeit = Zeit.split(":")
+    h = int (zeit[0])
+    m = int (zeit[1])
+    s = int (zeit[2])
     gesamt += h*3600
     gesamt += m*60
     gesamt += s
     return gesamt
 beginnZeit = input("Beginnzeit:\n")
 endZeit = input("Endzeit:")
-beginn = beginnZeit.split(":")
 end = endZeit.split(":")
-h1 = int (end[0])
-m1 = int (end[1])
-s1 = int (end[2])
-print (beginn)
-h = int (beginn[0])
-m = int (beginn[1])
-s = int (beginn[2])
-print("Stunden: h\n",h)
-print("Minuten: m\n",m)
-print("Sekunden: \n",s)
-beginnSekunden = zeitInSekunden(h, m, s)
-endSekunden = zeitInSekunden(h1, m1, s1)
-print (endSekunden-beginnSekunden)
+
+beginnSekunden = zeitInSekunden(beginnZeit)
+endSekunden = zeitInSekunden(Endzeit)
+print (endSekunden-beginnSekunden) 
+x=0
+while (wantstosubmit == true):
+    submission[x] = input("UserID")
+    x+=1
+    submission[x] = input("Zeit")
+    submission[x] = beginnSekunden-zeitInSekunden(submission[x])
+    x+=1
+    submission[x] = input("Correct/False")
+    x+=1
+for i in range(1,x+1,3):
+    print(submission[i])
+    print (submission[i+1])
+    print (submission[i+2])
