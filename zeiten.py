661af59677afa483ca427da050a8058e4e31a6a1
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-25 10:56:36 +0200

    e

--- zeiten.py
+++ zeiten.py
@@ -1,8 +1,17 @@
 # zeiten.py
 def zeitInSekunden(h, m, s):
+    gesamt = 0
+    gesamt += h*3600
+    gesamt += m*60
+    gesamt += s
+    return gesamt
 beginnZeit = input("Beginnzeit:\n")
 endZeit = input("Endzeit:")
 beginn = beginnZeit.split(":")
+end = endZeit.split(":")
+h1 = int (end[0])
+m1 = int (end[1])
+s1 = int (end[2])
 print (beginn)
 h = int (beginn[0])
 m = int (beginn[1])
@@ -10,5 +19,6 @@ s = int (beginn[2])
 print("Stunden: h\n",h)
 print("Minuten: m\n",m)
 print("Sekunden: \n",s)
-beginnSekunden = s + m*60 +h*3600
-print (beginnSekunden)
+beginnSekunden = zeitInSekunden(h, m, s)
+endSekunden = zeitInSekunden(h1, m1, s1)
+print (endSekunden-beginnSekunden)
