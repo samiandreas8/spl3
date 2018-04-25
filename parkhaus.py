a09b1b0dd904ee7f46da3e37b57c636aa65fb0b5
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-25 08:59:17 +0200

    Parkhaus Programmiert

--- parkhaus.py
+++ parkhaus.py
@@ -1,2 +1,11 @@
 # parkhaus.py 
 # geändert
+
+print ("Linienbus-Simulator")
+
+personen = 0
+haltestellen = int(input("wie viele Haltestellen soll es geben?"))
+for x in range (1,haltestellen+1):
+    personen = int(input("Haltestelle",x,"erreicht. Wie viele Personen steigen ein?")) + personen  
+    personen = personen - int(input("Haltestelle",x,"erreicht. Wie viele Personen steigen aus?"))
+    print("Es sind",personen,"im Bus.")
