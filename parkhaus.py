35dfd1e74fca4196dc1dc96b370a392dc61649a7
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-25 09:06:49 +0200

    Parkhaus Programmier2t

--- parkhaus.py
+++ parkhaus.py
@@ -6,6 +6,16 @@ print ("Linienbus-Simulator")
 personen = 0
 haltestellen = int(input("wie viele Haltestellen soll es geben?"))
 for x in range (1,haltestellen+1):
-    personen = int(input("Haltestelle",x,"erreicht. Wie viele Personen steigen ein?")) + personen  
-    personen = personen - int(input("Haltestelle",x,"erreicht. Wie viele Personen steigen aus?"))
+    einsteiger = int(input("Haltestelle",x,"erreicht. Wie viele Personen steigen ein?"))
+    if (personen + einsteiger > 60):
+        personen =  einsteiger + personen
+    else:
+        print("Es können",-60+personen+einsteiger,"einsteigen.") 
+        personen = 60
+    aussteiger =  int(input("Haltestelle",x,"erreicht. Wie viele Personen steigen aus?"))
+    if (personen - aussteiger >= 0):
+        personen = personen - aussteiger
+    else:
+        print("Es sind zu wenig Personen im Bus")
+        personen = 0
     print("Es sind",personen,"im Bus.")
