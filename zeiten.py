e0e215dcbc676e91b518a10a582bc16a554fbe46
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-25 12:27:10 +0200

    Zeiten mit sys.argv

--- zeiten.py
+++ zeiten.py
@@ -9,21 +9,9 @@ def zeitInSekunden(Zeit):
     gesamt += m*60
     gesamt += s
     return gesamt
-beginnZeit = input("Beginnzeit:\n")
-endZeit = input("Endzeit:")
+p = sys.argv
+
 end = endZeit.split(":")
-beginnSekunden = zeitInSekunden(beginnZeit)
-endSekunden = zeitInSekunden(Endzeit)
+beginnSekunden = zeitInSekunden(p[1])
+endSekunden = zeitInSekunden(p[2])
 print (endSekunden-beginnSekunden) 
-x=0
-while (wantstosubmit == true):
-    submission[x] = input("UserID")
-    x+=1
-    submission[x] = input("Zeit")
-    submission[x] = beginnSekunden-zeitInSekunden(submission[x])
-    x+=1
-    submission[x] = input("Correct/False")
-    x+=1
-for i in range(2,x+1,3):
-    print(submission[i])
-    
