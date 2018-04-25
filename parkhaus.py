566385a241518d35cb0df4e717b7150184439a5a
samiandreas8 <171witb11@o365.htl-leoben.at>
2018-04-25 09:19:10 +0200

    Parkhaus vertiggestellt

--- parkhaus.py
+++ parkhaus.py
@@ -4,18 +4,21 @@
 print ("Linienbus-Simulator")
 
 personen = 0
-haltestellen = int(input("wie viele Haltestellen soll es geben?"))
+haltestellen = int(input("wie viele Haltestellen soll es geben?\n"))
+
 for x in range (1,haltestellen+1):
-    einsteiger = int(input("Haltestelle",x,"erreicht. Wie viele Personen steigen ein?"))
-    if (personen + einsteiger > 60):
+    est = "Haltestelle "+ str(x)+ " erreicht. Wie viele Personen steigen ein?\n"
+    ast = "Wie viele Personen steigen aus?\n"
+    einsteiger = int(input(est))
+    if (personen + einsteiger <= 60):
         personen =  einsteiger + personen
     else:
-        print("Es können",-60+personen+einsteiger,"einsteigen.") 
+        print("Es können",-60+personen+einsteiger," nichteinsteigen.") 
         personen = 60
-    aussteiger =  int(input("Haltestelle",x,"erreicht. Wie viele Personen steigen aus?"))
+    aussteiger =  int(input(ast))
     if (personen - aussteiger >= 0):
         personen = personen - aussteiger
     else:
         print("Es sind zu wenig Personen im Bus")
         personen = 0
-    print("Es sind",personen,"im Bus.")
+    print("Es sind",personen,"im Bus.",einsteiger,"sind eingestiegen,",aussteiger,"sind ausgestiegen")
